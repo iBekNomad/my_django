2bf87407b4780efb17e4a2aff9461d4b14525dbc
Aibek <nomad.aibek@gmail.com>
2022-08-20 17:07:55 +0600

    updated article create view. Added adding article to site.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -14,10 +14,12 @@ def article_create_view(request):
     if request.method == 'GET':
         return render(request, 'article_create.html')
     elif request.method == 'POST':
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        author = request.POST.get('author')
+        article = Article.objects.create(title=title, text=text, author=author)
         context = {
-            'title': request.POST.get('title'),
-            'content': request.POST.get('content'),
-            'author': request.POST.get('author')
+            'article': article
         }
         return render(request, 'article_view.html', context)
 
